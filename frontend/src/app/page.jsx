c0e33fbc0e5a2e3d7daf182729d24727056b8b45
MatsudaSaku <afuuree@yahoo.co.jp>
2024-03-30 13:13:32 +0900

    API機能の追加

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,17 +1,44 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./home.module.css";
 import { GenreList } from "./components/genreList";
 import { MoodList } from "./components/moodList";
-
+import { useState } from "react";
 
 export default function Home() {
+  const [selectedMood, setSelectedMood] = useState("");
+  const [selectedGenre, setSelectedGenre] = useState("");
+
+  const handleDecisionClick = () => {
+    if (selectedMood && selectedGenre) {
+      window.location.href = `http://localhost:3001/recommendList/?mood=${selectedMood}&genre=${selectedGenre}`;
+    }
+  };
+
   return (
-    <div className={styles.home}>
-      <h2 className={styles.headline}>なりたい気分を選んでください</h2>
-      <MoodList />
-      <h2 className={styles.headline}>ジャンルを選んでください</h2>
-      <GenreList />
-      <button className={styles.doneButton}>決定</button>
+    <div className={styles.area}>
+      <div className={styles.home}>
+        <h2 className={styles.headline}>なりたい気分を選んでください</h2>
+        <MoodList onMoodSelect={setSelectedMood} />
+        <h2 className={styles.headline}>ジャンルを選んでください</h2>
+        <GenreList onGenreSelect={setSelectedGenre} />
+        <button className={styles.doneButton} onClick={handleDecisionClick}>
+          決定
+        </button>
+      </div>
+      <ul className={styles.circles}>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
   );
 }
--- frontend/src/app/recommendList/page.jsx
+++ frontend/src/app/recommendList/page.jsx
@@ -3,90 +3,153 @@
 import React, { useState, useEffect } from "react";
 import styles from "./styles.module.css";
 import Color, { Palette } from "color-thief-react";
+import { useSearchParams } from "next/navigation";
 
 export default function RecommendList() {
-    const [tracks, setTracks] = useState([]);
-    const [activeTrack, setActiveTrack] = useState(null);
-    const [showModal, setShowModal] = useState(false);
-    const [modalBackground, setModalBackground] = useState(null);
-    const [gradientAngle, setGradientAngle] = useState(45);
-    const accessToken = "「ご自身のアクセストークンを入力してください」";
+  const [tracks, setTracks] = useState([]);
+  const [activeTrack, setActiveTrack] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [modalBackground, setModalBackground] = useState(null);
+  const [gradientAngle, setGradientAngle] = useState(45);
+  const accessToken = "「ご自身のアクセストークンを入力してください」";
 
-    useEffect(() => {
-        if (accessToken) {
-            const fetchTracks = async () => {
-                try {
-                    const response = await fetch("https://api.spotify.com/v1/recommendations?limit=10&market=JP&seed_tracks=5Iy2Jj87Ha0C0IBlNE1I4y", {
-                        headers: {
-                            Authorization: `Bearer ${accessToken}`,
-                        },
-                    });
-                    const data = await response.json();
-                    setTracks(data.tracks);
-                } catch (error) {
-                    console.error("Error fetching tracks", error);
-                }
-            };
+  const searchParams = useSearchParams();
 
-            fetchTracks();
+  const mood = searchParams.get("mood");
+  const genre = searchParams.get("genre");
+
+  let url = `https://api.spotify.com/v1/recommendations?limit=12&seed_genres=${genre}`;
+
+  if (genre == "instrumentalness") {
+    url = `https://api.spotify.com/v1/recommendations?limit=12&seed_tracks=5Iy2Jj87Ha0C0IBlNE1I4y&min_instrumentalness=0.7`;
+  }
+
+  switch (mood) {
+    case "元気":
+      url += `&min_tempo=150&max_tempo=200&min_energy=0.7&max_energy=1.0`;
+      break;
+    case "しみじみ":
+      url += `&min_tempo=60&max_tempo=90&min_energy=0&max_energy=0.4`;
+      break;
+    case "おだやか":
+      url += `&min_tempo=60&max_tempo=90&min_energy=0&max_energy=0.4&max_loudness=-10`;
+      break;
+    case "うれしい":
+      url += `&min_valence=0.7`;
+      break;
+    case "ノスタルジック":
+      url += `&mode=0`;
+      break;
+    case "前向き":
+      url += `&min_valence=0.7&mode=1`;
+      break;
+    case "泣ける":
+      url += `&max_valence=0.4&mode=0`;
+      break;
+    case "ワクワク":
+      url += `&min_valence=0.7&mode=1`;
+      break;
+    case "落ち着く":
+      url += `&min_tempo=60&max_tempo=90&max_energy=0.4&max_loudness=-10`;
+      break;
+    case "ダンス":
+      url += `&danceability=1.0`;
+      break;
+  }
+
+  useEffect(() => {
+    if (accessToken) {
+      const fetchTracks = async () => {
+        try {
+          const response = await fetch(url, {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          });
+          const data = await response.json();
+          setTracks(data.tracks);
+        } catch (error) {
+          console.error("Error fetching tracks", error);
         }
-    }, [accessToken]);
+      };
 
-    // 角度を更新するための useEffect
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setGradientAngle(prevAngle => (prevAngle + 2) % 360);
-        }, 100);
+      fetchTracks();
+    }
+  }, [accessToken, searchParams]);
 
-        return () => clearInterval(intervalId); // コンポーネントがアンマウントされた時にクリア
-    }, []);
+  // 角度を更新するための useEffect
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setGradientAngle((prevAngle) => (prevAngle + 2) % 360);
+    }, 100);
 
-    const openModal = (track) => {
-        setActiveTrack(track);
-        setShowModal(true);
-        setModalBackground(null); // モーダルを開くたびに背景色をリセット
-    };
+    return () => clearInterval(intervalId); // コンポーネントがアンマウントされた時にクリア
+  }, []);
 
-    return (
-        <div>
-            <h1>おすすめの曲</h1>
-            <ul className={styles.recommendList}>
-                {tracks.map((track) => (
-                    <li key={track.id} onClick={() => openModal(track)}>
-                        <img src={track.album.images[0].url} alt={track.name} />
-                        <div>
-                            <h2>{track.name}</h2>
-                            <p>{track.artists.map((artist) => artist.name).join(", ")}</p>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-            {showModal && (
-                <>
-                    <div
-                        className={styles.modal}
-                        style={{
-                            background: modalBackground,
-                        }}
-                    >
-                        <iframe src={`https://open.spotify.com/embed/track/${activeTrack?.id}`} width="300" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-                        <button className={styles.closeButton} onClick={() => setShowModal(false)}>
-                            CLOSE
-                        </button>
-                    </div>
-                    <Palette src={activeTrack?.album.images[0].url} crossOrigin="anonymous" format="hex" colorCount={3}>
-                        {({ data, loading }) => {
-                            if (!loading && data) {
-                                // グラデーション文字列を生成
-                                const gradient = `linear-gradient(${gradientAngle}deg, ${data.join(",")})`;
-                                // 背景色をセット
-                                setModalBackground(gradient);
-                            }
-                            return null;
-                        }}
-                    </Palette>
-                </>
-            )}
-        </div>
-    );
+  const openModal = (track) => {
+    setActiveTrack(track);
+    setShowModal(true);
+    setModalBackground(null); // モーダルを開くたびに背景色をリセット
+  };
+
+  return (
+    <div>
+      <h1>おすすめの曲</h1>
+      <ul className={styles.recommendList}>
+        {tracks.map((track) => (
+          <li key={track.id} onClick={() => openModal(track)}>
+            <img src={track.album.images[0].url} alt={track.name} />
+            <div>
+              <h2>{track.name}</h2>
+              <p>{track.artists.map((artist) => artist.name).join(", ")}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+      {showModal && (
+        <>
+          <div
+            className={styles.modal}
+            style={{
+              background: modalBackground,
+            }}
+          >
+            <iframe
+              src={`https://open.spotify.com/embed/track/${activeTrack?.id}`}
+              width="300"
+              height="380"
+              frameBorder="0"
+              allowtransparency="true"
+              allow="encrypted-media"
+            ></iframe>
+            <button
+              className={styles.closeButton}
+              onClick={() => setShowModal(false)}
+            >
+              CLOSE
+            </button>
+          </div>
+          <Palette
+            src={activeTrack?.album.images[0].url}
+            crossOrigin="anonymous"
+            format="hex"
+            colorCount={3}
+          >
+            {({ data, loading }) => {
+              if (!loading && data) {
+                // グラデーション文字列を生成
+                const gradient = `linear-gradient(${gradientAngle}deg, ${data.join(
+                  ","
+                )})`;
+                // 背景色をセット
+                setModalBackground(gradient);
+              }
+              return null;
+            }}
+          </Palette>
+        </>
+      )}
+    </div>
+  );
 }
